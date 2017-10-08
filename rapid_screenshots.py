34dbea2f71ca2d6ad1a8964bfb2e0e8c957b45e9
unknown <yahna.3@wright.edu>
2017-10-08 15:13:37 -0400

    Cleaned up the code a little. CTRL+C signal is still not working correctly

--- rapid_screenshots.py
+++ rapid_screenshots.py
@@ -3,12 +3,18 @@
 #ERRORS. The kill signal might be going to the child thread or something. Issue 846817 on https://bugs.python.org/issue846817
 import pyscreenshot
 from timeit import default_timer as timer
-from Queue import Queue
+from Queue import Queue, Empty
 from threading import Thread
 import threading
 from time import sleep
-#import signal
+import signal
 #import sys
+
+#Global variables
+DEFAULT_TIME_LIMIT = 10
+DEFAULT_LOOP_TIME_LIMIT = 0
+DEFAULT_SCREENSHOTS_MAX = 0
+
 
 class KillSignalHandler:
     def __init__(self, image_producer_thread, image_consumer_thread):
@@ -17,13 +23,12 @@ class KillSignalHandler:
     def kill_signal_handler(self, signal, frame):
         print('You pressed CTRL+C')
         self.image_producer_thread.stop()
-        self.image_producer_thread.join()
-        self.image_consumer_thread.join()
-        sys.exit(0)
+        self.image_consumer_thread.stop()
+        #self.image_producer_thread.join()
+        #self.image_consumer_thread.join()
+        #sys.exit(0)
 
-DEFAULT_TIME_LIMIT = 10
-DEFAULT_LOOP_TIME_LIMIT = 0
-DEFAULT_SCREENSHOTS_MAX = 0
+
 class ScreenshotProducerThread(Thread):
     def __init__(self, img_queue, consumer_process, time_limit):
         Thread.__init__(self)
@@ -34,15 +39,13 @@ class ScreenshotProducerThread(Thread):
     def run(self):
         start_time = timer()
         new_time = timer()
-        while(new_time - start_time < self.time_limit or self.time_limit <= 0 and not self.stop_event.is_set()):
+        while((new_time - start_time < self.time_limit or self.time_limit <= 0) and not self.stop_event.is_set()):
             img = pyscreenshot.grab()
             #send to another process
-            ##imgnum = imgnum + 1
-            ##img.save(str(imgnum) + '.png')
             self.img_queue.put(img)
             new_time = timer()
         self.consumer_process.stop()
-        #self.consumer_process.join()
+
     def stop(self):
         """
         Stop the producer process
@@ -77,7 +80,8 @@ class ImageConsumerThread(Thread):
                         print('timed for a new loop: timer: ' + str(timer() - start_timer))
                         imgnum = 1
                         start_timer = timer()
-                except Queue.Empty:
+                except Empty:
+                    #shouldn't reach here since .empty() is checked
                     print('the queue is empty')
             sleep(1)
     
@@ -97,29 +101,28 @@ def main_method(time_limit, loop_time_limit, screenshots_max):
     @param screenshots_max an integer value indicating the maximum number of pictures to keep
     if it is set to zero or less there will be no limit
     """
-    start_time = timer()
-    new_time = timer()
-    #imgnum = 0
     img_queue = Queue()
     consumer_process = ImageConsumerThread(img_queue, loop_time_limit, screenshots_max)
-    consumer_process.start()
+    consumer_process.daemon = True
     producer_process = ScreenshotProducerThread(img_queue, consumer_process, time_limit)
-    producer_process.start()
-    #ksh = KillSignalHandler(producer_process, consumer_process)
-    #signal.signal(signal.SIGINT, ksh.kill_signal_handler)
-    #print(threading.enumerate())
-    #producer_process.join()
-    #consumer_process.join()
-    try:
-        #producer_process.join()
-        #consumer_process.join()
-        while(True):
-            sleep(1)
+    producer_process.daemon = True
+    ksh = KillSignalHandler(producer_process, consumer_process)
+    signal.signal(signal.SIGINT, ksh.kill_signal_handler)
+    
+    consumer_process.start()
+    producer_process.start()    
+    producer_process.join()
+    consumer_process.join()
+    '''try:
+        producer_process.join()
+        consumer_process.join()
+        #while(True):
+            #sleep(1)
     except KeyboardInterrupt:
         print('pressed CTRL+C')
         producer_process.stop()
         producer_process.join()
-        consumer_process.join()
+        consumer_process.join()'''
 
 if __name__ == '__main__':
     #rapidly take screenshots and queue them to be saved as images
