20a14605e548d1fe72b135a50d3378058a7e0d70
unknown <yahna.3@wright.edu>
2017-10-08 20:15:31 -0400

    Added comments

--- rapid_screenshots.py
+++ rapid_screenshots.py
@@ -9,19 +9,32 @@ import threading
 from time import sleep
 import signal
 import argparse
-#import sys
 
 #Global variables
-DEFAULT_TIME_LIMIT = 10
+DEFAULT_TIME_LIMIT = 10 #runs for ten seconds
 DEFAULT_LOOP_TIME_LIMIT = 0
 DEFAULT_SCREENSHOTS_MAX = 0
 
 
 class KillSignalHandler:
+    """
+    A class that has a method for handling a signal.
+    """
     def __init__(self, image_producer_thread, image_consumer_thread):
+        """
+        The initialization method
+        @param image_producer_thread a thread with the stop method
+        @param image_consumer_thread a thread with the stop method
+        """
         self.image_consumer_thread = image_consumer_thread
         self.image_producer_thread = image_producer_thread
     def kill_signal_handler(self, signal, frame):
+        """
+        A method that closes the two threads given to the object
+        and can be used for a signal callback
+        @param signal the signal received
+        @param frame a frame
+        """
         print('You pressed CTRL+C')
         self.image_producer_thread.stop()
         self.image_consumer_thread.stop()
@@ -31,13 +44,28 @@ class KillSignalHandler:
 
 
 class ScreenshotProducerThread(Thread):
+    """
+    A thread that produces image objects from screenshots
+    and puts them in a queue
+    """
     def __init__(self, img_queue, consumer_process, time_limit):
+        """
+        initialization method
+        @param img_queue a Queue object for storing images
+        @param consumer_process an ImageConsumerThread that this 
+        thread will call .stop() on when finished
+        @time_limit a number in seconds that determines how long this
+        thread runs. if it is 0 or less then the process runs forever
+        """
         Thread.__init__(self)
         self.img_queue = img_queue
         self.consumer_process = consumer_process
         self.time_limit = time_limit
         self.stop_event = threading.Event()
     def run(self):
+        """
+        Run the process
+        """
         start_time = timer()
         new_time = timer()
         while((new_time - start_time < self.time_limit or self.time_limit <= 0) and not self.stop_event.is_set()):
@@ -54,7 +82,19 @@ class ScreenshotProducerThread(Thread):
         self.stop_event.set()
 
 class ImageConsumerThread(Thread):
+    """
+    A thread that takes images from a queue and saves them as .png files
+    """
     def __init__(self, imgQueue, loop_time_limit, images_max):
+        """
+        The initialization method
+        @imgQueue a Queue object to take images from
+        @loop_time_limit a number that denotes when to
+        start overwriting old images to save space
+        if it is 0 or less it is ignored
+        @images_max an integer indicating how many images to store
+        if it is 0 or less it is ignored
+        """
         Thread.__init__(self)
         self.imgQueue = imgQueue
         self.loop_time_limit = loop_time_limit
@@ -62,6 +102,9 @@ class ImageConsumerThread(Thread):
         self.stop_event = threading.Event()
 
     def run(self):
+        """
+        Runs the thread
+        """
         #check if there are images to consume
         #if not, then sleep
         imgnum = 1
